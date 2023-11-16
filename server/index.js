4e6e69909826ed029b9cd64c5b56cf77795a9703
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-16 21:49:49 +0300

    add modules and connect router

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
+const models = require("./models/models");
+const cors = require("cors"); // нужен для запросов из браузера
+const router = require("./routes/index");
 
 const PORT = process.env.PORT || 5000; // Получаем порт из переменных окружения
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/", router);
 
 const start = async () => {
   try {

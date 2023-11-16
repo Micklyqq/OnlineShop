152c00aeee26d7f0f8f0aba701bb744bcd301413
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-16 20:57:24 +0300

    fix dotenv, add bd auth

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
-require("dotenv").config;
+require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db");
 
 const PORT = process.env.PORT || 5000; // Получаем порт из переменных окружения
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server is starting on port ${PORT}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server is starting on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();

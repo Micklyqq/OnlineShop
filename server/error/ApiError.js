60ed899e07aed40d02c131b1a46ee7656dbe1d77
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-17 15:51:29 +0300

    add error handler middleware

---
+++ server/error/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+  constructor(status, message) {
+    super(); // вызываем родительский конструктор
+    this.status = status;
+    this.message = message;
+  }
+
+  static badRequest(message) {
+    // запрос прошел, но сервер не может найти данные согласно запросу
+    return new ApiError(404, message);
+  }
+
+  static internal(message) {
+    // внутренняя проблема сервера
+    return new ApiError(500, message);
+  }
+
+  static badRequest(message) {
+    // нет доступа
+    return new ApiError(404, message);
+  }
+}
+
+module.exports = ApiError;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors"); // нужен для запросов из браузера
 const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 
 const PORT = process.env.PORT || 5000; // Получаем порт из переменных окружения
 
@@ -11,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/", router);
+
+// Обработка ошибок, последний Middleware(ставим последним тк в случае если на предыдущих middleware будет ошибка, то express вызывает последний middleware)
+app.use(errorHandler);
 
 const start = async () => {
   try {

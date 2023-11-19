3cf4d366fba0f3748a1951f178b04dcc8c2fe53d
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-19 21:24:07 +0300

    create route system

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { Routes, Navigate, Route } from "react-router-dom";
+import { authRoutes, publicRoutes } from "../routes";
+import { SHOP_ROUTE } from "../utils/consts";
+import { Context } from "../index";
+
+export const AppRouter = () => {
+  const { user } = useContext(Context);
+  console.log(user);
+  return (
+    <Routes>
+      {user.isAuth &&
+        authRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} />
+      ))}
+      <Route path="*" element={<Navigate to={SHOP_ROUTE} replace />} />
+    </Routes>
+  );
+};

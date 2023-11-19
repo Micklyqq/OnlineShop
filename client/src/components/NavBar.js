055d59fc8aae6c2321ed8e212930c0b798827624
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-20 00:25:22 +0300

    add login and admin routes in nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,12 +4,14 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
-import { SHOP_ROUTE } from "../utils/consts";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import { Button } from "react-bootstrap";
 import { observer } from "mobx-react-lite";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
+  const navigate = useNavigate();
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
@@ -18,22 +20,22 @@ const NavBar = observer(() => {
         </NavLink>
         {user.isAuth ? (
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button variant={"outline-light"}>Админ панель</Button>
             <Button
               variant={"outline-light"}
-              onClick={() => user.setIsAuth(false)}
+              onClick={() => navigate(ADMIN_ROUTE)}
+            >
+              Админ панель
+            </Button>
+            <Button
+              variant={"outline-light"}
+              onClick={() => navigate(LOGIN_ROUTE)}
             >
               Выйти
             </Button>
           </Nav>
         ) : (
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button
-              variant={"outline-light"}
-              onClick={() => user.setIsAuth(true)}
-            >
-              Авторизация
-            </Button>
+            <Button variant={"outline-light"}>Авторизация</Button>
           </Nav>
         )}
       </Container>

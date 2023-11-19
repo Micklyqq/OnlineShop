c681aab94507637cdf983d755e8700368253ce91
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-19 21:24:55 +0300

    create shop page

---
+++ client/src/components/DeviceList.js
@@ -0,0 +1,17 @@
+import { observer } from "mobx-react-lite";
+import React, { useContext } from "react";
+import { Context } from "../index";
+import { DeviceItem } from "./DeviceItem";
+import { Row } from "react-bootstrap";
+
+export const DeviceList = observer(() => {
+  const { device } = useContext(Context);
+
+  return (
+    <Row className="d-flex">
+      {device.devices.map((device) => (
+        <DeviceItem key={device.id} device={device} />
+      ))}
+    </Row>
+  );
+});
